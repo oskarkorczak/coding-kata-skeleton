4ec18fd5c95e55e6b868f115f583c3019cf583ea
Oskar Korczak <o.korczak@gmail.com>
2025-05-20 22:22:06 +0200

    Red: Failing test for divisibility by 3

--- src/test/java/EvilFizzBuzzTest.java
+++ src/test/java/EvilFizzBuzzTest.java
@@ -8,10 +8,10 @@ public class EvilFizzBuzzTest {
     private final EvilFizzBuzz evilFizzBuzz = new EvilFizzBuzz();
 
     @Test
-    void shouldGenerateIntegersSequence() {
-        var seq = evilFizzBuzz.generate(5);
+    void shouldDetectIntegersDivisibleBy3() {
+        var seq = evilFizzBuzz.generate(7);
 
-        assertThat(seq).isEqualTo(EvilFizzBuzz.HARD_CODED_SEQ);
+        assertThat(seq).isEqualTo("1, 2, Fizz, 4, 5, Fizz, 7");
     }
 
     @Disabled("ATDD test should run when full implementation in place.")
