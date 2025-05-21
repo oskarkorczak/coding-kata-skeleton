8abf550ec09c37dbacbf7c4631022c39409a21ab
Oskar Korczak <o.korczak@gmail.com>
2025-05-21 21:23:12 +0200

    Green: Implementation for divisibility by 15

--- src/main/java/EvilFizzBuzz.java
+++ src/main/java/EvilFizzBuzz.java
@@ -7,12 +7,14 @@ public class EvilFizzBuzz {
 
     static final Predicate<Integer> isDivisibleBy3Predicate = i -> i % 3 == 0;
     static final Predicate<Integer> isDivisibleBy5Predicate = i -> i % 5 == 0;
+    static final Predicate<Integer> isDivisibleBy15Predicate = i -> isDivisibleBy3Predicate.test(i) && isDivisibleBy5Predicate.test(i);
 
     public String generate(int limit) {
         return IntStream
                 .rangeClosed(1, limit)
                 .boxed()
                 .map( n -> switch (n) {
+                    case Integer i when isDivisibleBy15Predicate.test(i) -> "FizzBuzz";
                     case Integer i when isDivisibleBy3Predicate.test(i) -> "Fizz";
                     case Integer i when isDivisibleBy5Predicate.test(i) -> "Buzz";
                     default -> String.valueOf(n);
