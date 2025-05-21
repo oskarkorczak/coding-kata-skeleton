898ca8bc76cf93f11359118720f35a096c49fa50
Oskar Korczak <o.korczak@gmail.com>
2025-05-21 22:19:33 +0200

    Refactor: Extract prime check to separate method

--- src/main/java/EvilFizzBuzz.java
+++ src/main/java/EvilFizzBuzz.java
@@ -1,45 +1,37 @@
 import java.util.function.Predicate;
 import java.util.stream.IntStream;
 
+import static java.lang.Math.floor;
+import static java.lang.Math.sqrt;
 import static java.util.stream.Collectors.joining;
 
 public class EvilFizzBuzz {
-
-    static final Predicate<Integer> isDivisibleBy3Predicate = i -> i % 3 == 0;
-    static final Predicate<Integer> isDivisibleBy5Predicate = i -> i % 5 == 0;
-    static final Predicate<Integer> isDivisibleBy15Predicate = i -> isDivisibleBy3Predicate.test(i) && isDivisibleBy5Predicate.test(i);
-    static final Predicate<Integer> isPrime = value -> {
-        return value > 1 && IntStream
-                .rangeClosed(2, (int) Math.floor(Math.sqrt(value)))
-                .noneMatch(divisor -> value % divisor == 0);
-    };
 
     public String generate(int limit) {
         return IntStream
                 .rangeClosed(1, limit)
                 .boxed()
-                .map( n -> switch (n) {
-                    case Integer i when isDivisibleBy15Predicate.test(i) -> {
-                        if (isPrime.test(i)) {
-                            yield "FizzBuzzWizz";
-                        }
-                        yield "FizzBuzz";
-                    }
-                    case Integer i when isDivisibleBy3Predicate.test(i) -> {
-                        if (isPrime.test(i)) {
-                            yield "FizzWizz";
-                        }
-                        yield "Fizz";
-                    }
-                    case Integer i when isDivisibleBy5Predicate.test(i) -> {
-                        if (isPrime.test(i)) {
-                            yield "BuzzWizz";
-                        }
-                        yield "Buzz";
-                    }
+                .map(n -> switch (n) {
+                    case Integer i when isDivisibleBy15.test(i) -> toLabel(i, "FizzBuzz");
+                    case Integer i when isDivisibleBy3.test(i) -> toLabel(i, "Fizz");
+                    case Integer i when isDivisibleBy5.test(i) -> toLabel(i, "Buzz");
                     case Integer i when isPrime.test(i) -> "Wizz";
                     default -> String.valueOf(n);
                 })
                 .collect(joining(", "));
+    }
+
+    static final Predicate<Integer> isDivisibleBy3 = i -> i % 3 == 0;
+    static final Predicate<Integer> isDivisibleBy5 = i -> i % 5 == 0;
+    static final Predicate<Integer> isDivisibleBy15 = i -> isDivisibleBy3.test(i) && isDivisibleBy5.test(i);
+    static final Predicate<Integer> isPrime = value -> value > 1 && IntStream
+            .rangeClosed(2, (int) floor(sqrt(value)))
+            .noneMatch(divisor -> value % divisor == 0);
+
+    private static String toLabel(int value, String label) {
+        if (isPrime.test(value)) {
+            return label + "Wizz";
+        }
+        return label;
     }
 }
--- src/test/java/EvilFizzBuzzTest.java
+++ src/test/java/EvilFizzBuzzTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -31,36 +30,36 @@ public class EvilFizzBuzzTest {
     @ParameterizedTest
     @ValueSource(ints = {-6, -3, 0, 3, 6})
     void shouldDetectIntegersDivisibleBy3(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy3Predicate.test(value)).isTrue();
+        assertThat(EvilFizzBuzz.isDivisibleBy3.test(value)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-2, -1, 1, 4, 7})
     void shouldNotDetectIntegersDivisibleBy3(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy3Predicate.test(value)).isFalse();
+        assertThat(EvilFizzBuzz.isDivisibleBy3.test(value)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-10, -5, 0, 5, 10})
     void shouldDetectIntegersDivisibleBy5(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy5Predicate.test(value)).isTrue();
+        assertThat(EvilFizzBuzz.isDivisibleBy5.test(value)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-11, -7, 1, 3, 7})
     void shouldNotDetectIntegersDivisibleBy5(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy5Predicate.test(value)).isFalse();
+        assertThat(EvilFizzBuzz.isDivisibleBy5.test(value)).isFalse();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-30, -15, 0, 15, 30})
     void shouldDetectIntegersDivisibleBy15(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy15Predicate.test(value)).isTrue();
+        assertThat(EvilFizzBuzz.isDivisibleBy15.test(value)).isTrue();
     }
 
     @ParameterizedTest
     @ValueSource(ints = {-31, -27, 1, 13, 17, 31})
     void shouldNotDetectIntegersDivisibleBy15(int value) {
-        assertThat(EvilFizzBuzz.isDivisibleBy15Predicate.test(value)).isFalse();
+        assertThat(EvilFizzBuzz.isDivisibleBy15.test(value)).isFalse();
     }
 }
