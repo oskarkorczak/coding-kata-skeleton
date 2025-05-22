2c8cde424ab18213b905f3aaf4d71d60a6209d7e
Oskar Korczak <o.korczak@gmail.com>
2025-05-22 21:06:18 +0200

    Refactor: Collapse all prime detection tests into a single method source parametrised test

--- src/test/java/EvilFizzBuzzTest.java
+++ src/test/java/EvilFizzBuzzTest.java
@@ -22,8 +22,8 @@ public class EvilFizzBuzzTest {
 
     @ParameterizedTest
     @MethodSource("divisibilityDetectionProvider")
-    void divisibilityDetection(Integer value, Predicate<Integer> predicate, boolean expected) {
-        assertThat(predicate.test(value)).isEqualTo(expected);
+    void detectsDivisibilityByNumber(Integer number, Predicate<Integer> predicate, boolean expected) {
+        assertThat(predicate.test(number)).isEqualTo(expected);
     }
 
     static Stream<Arguments> divisibilityDetectionProvider() {
@@ -73,14 +73,33 @@ public class EvilFizzBuzzTest {
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {2, 3, 5, 7, 11, 13, 17, 23, 31})
-    void detectsPrimeNumbers(int value) {
-        assertThat(EvilFizzBuzz.isPrime.test(value)).isTrue();
+    @MethodSource("primeNumbersProvider")
+    void detectsPrimeNumbers(Integer number, Predicate<Integer> predicate, boolean expected) {
+        assertThat(predicate.test(number)).isEqualTo(expected);
     }
 
-    @ParameterizedTest
-    @ValueSource(ints = {-7, -1, 0, 1, 4, 6, 8, 9, 10, 20, 55, 100})
-    void doNotdetectPrimeNumbers(int value) {
-        assertThat(EvilFizzBuzz.isPrime.test(value)).isFalse();
+    static Stream<Arguments> primeNumbersProvider() {
+        return Stream.of(
+                Arguments.of(2, EvilFizzBuzz.isPrime, true),
+                Arguments.of(3, EvilFizzBuzz.isPrime, true),
+                Arguments.of(5, EvilFizzBuzz.isPrime, true),
+                Arguments.of(7, EvilFizzBuzz.isPrime, true),
+                Arguments.of(11, EvilFizzBuzz.isPrime, true),
+                Arguments.of(13, EvilFizzBuzz.isPrime, true),
+                Arguments.of(17, EvilFizzBuzz.isPrime, true),
+                Arguments.of(23, EvilFizzBuzz.isPrime, true),
+                Arguments.of(31, EvilFizzBuzz.isPrime, true),
+
+                Arguments.of(-10, EvilFizzBuzz.isPrime, false),
+                Arguments.of(-7, EvilFizzBuzz.isPrime, false),
+                Arguments.of(-1, EvilFizzBuzz.isPrime, false),
+                Arguments.of(0, EvilFizzBuzz.isPrime, false),
+                Arguments.of(1, EvilFizzBuzz.isPrime, false),
+                Arguments.of(4, EvilFizzBuzz.isPrime, false),
+                Arguments.of(6, EvilFizzBuzz.isPrime, false),
+                Arguments.of(14, EvilFizzBuzz.isPrime, false),
+                Arguments.of(15, EvilFizzBuzz.isPrime, false),
+                Arguments.of(16, EvilFizzBuzz.isPrime, false)
+        );
     }
 }
